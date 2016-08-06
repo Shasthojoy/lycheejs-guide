2c898dd2ce966fb39a126049943132d012a46469
Cookie Engineer <cookiengineer@protonmail.ch>
2016-08-06 23:18:33 +0200

    :bug: Fix

--- tutorials/15-fertilizer/bin/package.sh
+++ tutorials/15-fertilizer/bin/package.sh
@@ -3,6 +3,7 @@
 LYCHEEJS_ROOT=$(cd "$(dirname "$(readlink -f "$0")")/../../../"; pwd);
 PROJECT_ROOT=$(cd "$(dirname "$(readlink -f "$0")")/../"; pwd);
 PROJECT_BUILD="$1";
+PROJECT_TITLE="awesome-project";
 
 
 if [ -d $PROJECT_ROOT/build ]; then
@@ -12,7 +13,7 @@ if [ -d $PROJECT_ROOT/build ]; then
 	if [ "$PROJECT_BUILD" == "html/main" ]; then
 
 		cd "$PROJECT_ROOT/build/$PROJECT_BUILD";
-		zip -qr "$PROJECT_ROOT/build/lethalmaze_browser_all.zip" ./*;
+		zip -qr "$PROJECT_ROOT/build/${PROJECT_TITLE}_browser_all.zip" ./*;
 
 
 		rm -rf "$PROJECT_ROOT/build/$PROJECT_BUILD";
@@ -20,15 +21,15 @@ if [ -d $PROJECT_ROOT/build ]; then
 	elif [ "$PROJECT_BUILD" == "html-nwjs/main" ]; then
 
 		cd "$PROJECT_ROOT/build/$PROJECT_BUILD-linux/x86_64";
-		zip -qr "$PROJECT_ROOT/build/lethalmaze_linux_x86_64.zip" ./*;
+		zip -qr "$PROJECT_ROOT/build/${PROJECT_TITLE}_linux_x86_64.zip" ./*;
 		rm -rf "$PROJECT_ROOT/build/$PROJECT_BUILD-linux";
 
 		cd "$PROJECT_ROOT/build/$PROJECT_BUILD-osx/x86_64";
-		zip -qr "$PROJECT_ROOT/build/lethalmaze_osx_x86_64.zip" ./*;
+		zip -qr "$PROJECT_ROOT/build/${PROJECT_TITLE}_osx_x86_64.zip" ./*;
 		rm -rf "$PROJECT_ROOT/build/$PROJECT_BUILD-osx";
 
 		cd "$PROJECT_ROOT/build/$PROJECT_BUILD-windows/x86_64";
-		zip -qr "$PROJECT_ROOT/build/lethalmaze_windows_x86_64.zip" ./*;
+		zip -qr "$PROJECT_ROOT/build/${PROJECT_TITLE}_windows_x86_64.zip" ./*;
 		rm -rf "$PROJECT_ROOT/build/$PROJECT_BUILD-windows";
 
 
@@ -36,13 +37,13 @@ if [ -d $PROJECT_ROOT/build ]; then
 
 	elif [ "$PROJECT_BUILD" == "html-webview/main" ]; then
 
-		cp "$PROJECT_ROOT/build/$PROJECT_BUILD-android/app-release-unsigned.apk" "$PROJECT_ROOT/build/lethalmaze_android_all.apk";
+		cp "$PROJECT_ROOT/build/$PROJECT_BUILD-android/app-release-unsigned.apk" "$PROJECT_ROOT/build/${PROJECT_TITLE}_android_all.apk";
 		rm -rf "$PROJECT_ROOT/build/$PROJECT_BUILD-android";
 
-		cp "$PROJECT_ROOT/build/$PROJECT_BUILD-firefoxos/app.zip" "$PROJECT_ROOT/build/lethalmaze_firefoxos_all.zip";
+		cp "$PROJECT_ROOT/build/$PROJECT_BUILD-firefoxos/app.zip" "$PROJECT_ROOT/build/${PROJECT_TITLE}_firefoxos_all.zip";
 		rm -rf "$PROJECT_ROOT/build/$PROJECT_BUILD-firefoxos";
 
-		cp "$PROJECT_ROOT/build/$PROJECT_BUILD-ubuntu/lethalmaze-1.0.0-all.deb" "$PROJECT_ROOT/build/lethalmaze_ubuntutouch_all.deb";
+		cp "$PROJECT_ROOT/build/$PROJECT_BUILD-ubuntu/${PROJECT_TITLE}-1.0.0-all.deb" "$PROJECT_ROOT/build/${PROJECT_TITLE}_ubuntutouch_all.deb";
 		rm -rf "$PROJECT_ROOT/build/$PROJECT_BUILD-ubuntu";
 
 
